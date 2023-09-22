3538aca0b0a3c3944e08b45e0d0ec1f29825afad
Pratham9497 <prathams.ug21.cs@nitp.ac.in>
2023-09-22 16:17:52 +0530

    Add reset func

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -3,21 +3,21 @@ import Card from "../Card/Card"
 import './Grid.css'
 import isWinner from "../../helpers/checkWinner"
 
-function Grid({noc}) {
+function Grid({ noc }) {
     const [board, setBoard] = useState(Array(noc).fill(''))
     const [turn, setTurn] = useState(true) // true->O , false->X
     const [winner, setWinner] = useState(null)
 
     function playTurn(index) {
-        if(board[index]!="" || winner) return;
-        if(turn){
+        if (board[index] != "" || winner) return;
+        if (turn) {
             board[index] = 'O'
         }
-        else{
+        else {
             board[index] = 'X'
         }
-        let win = isWinner(board, turn?"O":"X")
-        if(win){
+        let win = isWinner(board, turn ? "O" : "X")
+        if (win) {
             setWinner(win);
         }
         setBoard([...board])
@@ -37,14 +37,14 @@ function Grid({noc}) {
                     winner && (
                         <>
                             <h1 className="player-turn"> Winner is {winner} </h1>
-                            <button className="reset" onClick={reset}>
-                                Reset Game
-                            </button>
                         </>
                     )
                 }
+                <button className="reset" onClick={reset}>
+                    Reset Game
+                </button>
                 <h1 className="player-turn">
-                    Current Turn : {turn?"O":"X"}
+                    Current Turn : {turn ? "O" : "X"}
                 </h1>
                 <div className="grid">
                     {board.map((el, idx) => <Card key={idx} onPlay={playTurn} player={el} index={idx} />)}
